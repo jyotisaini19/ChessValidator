c8e103bdea2c1e8576a7f011e506c63c3328b993
jyotisaini11 <jyoti.saini@swiggy.in>
2021-01-09 17:38:52 +0530

    Refactoring pawn code

--- src/piece/Pawn.java
+++ src/piece/Pawn.java
@@ -14,25 +14,20 @@ public class Pawn extends Piece
         int rowSize = cells.size();
         int colSize = cells.get(0).size();
 
-        if ((!source.isPositionWithInBoard(rowSize, colSize)) || (!destination.isPositionWithInBoard(rowSize, colSize))) {
+        if ((!source.isPositionWithInBoard(rowSize, colSize))
+                || (!destination.isPositionWithInBoard(rowSize, colSize))
+                || (this.color == Color.WHITE && destination.getRow() != (source.getRow()+1))
+                || (this.color == Color.BLACK && destination.getRow() != (source.getRow()-1))) {
             return false;
         }
 
-        if ( ((this.color == Color.WHITE) && (destination.getRow() < source.getRow()))
-                || (this.color== Color.BLACK && (destination.getRow() >  source.getRow()))
-                || (!(Math.abs(destination.getRow() - source.getRow()) == 1))) {
-            return false;
-        }
+        return (destination.getCol() == source.getCol() && cells.get(destination.getRow()).get(destination.getCol()).getPiece().equals("--"))
+                || (((destination.getCol() == source.getCol() - 1) || (destination.getCol() == source.getCol() + 1))
+                && isOpponent(cells, source, destination));
+    }
 
-        if ( Math.abs(destination.getCol() - source.getCol()) == 0) {
-            return (cells.get(destination.getRow()).get(destination.getCol()).getPiece().equals("--")
-                    || cells.get(destination.getRow()).get(destination.getCol()).getPiece().charAt(0) != cells.get(source.getRow()).get(source.getCol()).getPiece().charAt(0));
-        }
-
-        if (Math.abs(destination.getCol() - source.getCol()) == 1) {
-            return !(cells.get(destination.getRow()).get(destination.getCol()).getPiece().equals("--") || cells.get(destination.getRow()).get(destination.getCol()).getPiece().charAt(0) == cells.get(source.getRow()).get(source.getCol()).getPiece().charAt(0));
-        }
-
-        return false;
+    private boolean isOpponent(List<List<Cell>> cells , Position source, Position destination) {
+        return cells.get(destination.getRow()).get(destination.getCol()).getPiece().charAt(0)
+                != cells.get(source.getRow()).get(source.getCol()).getPiece().charAt(0);
     }
 }
